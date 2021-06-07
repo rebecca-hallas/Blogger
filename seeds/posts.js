5976da44b04ff0c23ad9d2dea0a287ab0ec86843
Rebecca-Hallas <rebeccajhallas@gmail.com>
2021-06-07 23:57:50 +1200

    add seed data and run seed files

--- seeds/posts.js
+++ seeds/posts.js
@@ -1,13 +1,36 @@
-
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('blogPosts').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+      return knex('blogPosts').insert([
+        {
+          id: 1,
+          title: 'My First Blog Post',
+          date_added: new Date(Date.now()),
+          comment_count: 0,
+          post_content: JSON.stringify([
+            'This is my very first blog post.'
+          ])
+        },
+        {
+          id: 2,
+          title: 'My Second Blog Post',
+          date_added: new Date(Date.now()),
+          comment_count: 0,
+          post_content: JSON.stringify([
+            'This is my second blog post.'
+          ])
+        },
+        {
+          id: 3,
+          title: 'My Third Blog Post',
+          date_added: new Date(Date.now()),
+          comment_count: 0,
+          post_content: JSON.stringify([
+            'This is my third blog post.'
+          ])
+        }
+      ])
+    })
+}
