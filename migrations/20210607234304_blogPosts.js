4596c1e9d5087bb9aee6499d27e7623790f6b006
Rebecca-Hallas <rebeccajhallas@gmail.com>
2021-06-07 23:51:04 +1200

    fill out blogPosts table

--- migrations/20210607234304_blogPosts.js
+++ migrations/20210607234304_blogPosts.js
@@ -1,8 +1,13 @@
+exports.up = (knex) => {
+  return knex.schema.createTable('blogPosts', (table) => {
+    table.increments().primary()
+    table.string('title')
+    table.date('date_added')
+    table.integer('comment_count').defaultsTo(0)
+    table.string('post_content')
+  })
+}
 
-exports.up = function(knex) {
-  
-};
-
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {
+  return knex.schema.dropTable('blogPosts')
+}
